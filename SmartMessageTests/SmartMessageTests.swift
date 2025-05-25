acbb9e487fff17063ca9b3639b2c8ebd3864394f
luosam1123 <luosam1123@gmail.com>
2025-05-25 09:09:46 +0800

    Initial Commit

---
+++ SmartMessageTests/SmartMessageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMessageTests.swift
+//  SmartMessageTests
+//
+//  Created by huiluo on 2025/5/25.
+//
+
+import Testing
+@testable import SmartMessage
+
+struct SmartMessageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
